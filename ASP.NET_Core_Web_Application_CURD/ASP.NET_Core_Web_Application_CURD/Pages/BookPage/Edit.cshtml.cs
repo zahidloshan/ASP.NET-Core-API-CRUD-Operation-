0b611599e6294b5f6757604fd43a0d756a66320f
loshan <zahidloshan@gmail.com>
2022-04-10 02:36:14 +0600

    Book Edit

---
+++ ASP.NET_Core_Web_Application_CURD/ASP.NET_Core_Web_Application_CURD/Pages/BookPage/Edit.cshtml.cs
@@ -0,0 +1,36 @@
+using ASP.NET_Core_Web_Application_CURD.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_Core_Web_Application_CURD.Pages.BookPage
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDBContext _db;
+        
+        public EditModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet( int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.Token = Book.Token;
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
