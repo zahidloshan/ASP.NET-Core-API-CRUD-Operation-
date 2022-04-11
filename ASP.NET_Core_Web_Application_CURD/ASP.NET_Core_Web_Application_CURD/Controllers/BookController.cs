20313f43200d641335779f78670d8c2780609e9d
zabir6093 <zabir6093@gmail.com>
2022-04-12 01:13:31 +0600

    Delete Book using ajax

--- ASP.NET_Core_Web_Application_CURD/ASP.NET_Core_Web_Application_CURD/Controllers/BookController.cs
+++ ASP.NET_Core_Web_Application_CURD/ASP.NET_Core_Web_Application_CURD/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using ASP.NET_Core_Web_Application_CURD.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASP.NET_Core_Web_Application_CURD.Controllers
 {
@@ -13,9 +14,23 @@ namespace ASP.NET_Core_Web_Application_CURD.Controllers
         {
             _db = db;
         }
-        public IActionResult GetAll()
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new {data=_db.Book.ToList()});
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
         }
     }
 }
--- ASP.NET_Core_Web_Application_CURD/ASP.NET_Core_Web_Application_CURD/wwwroot/js/BookList.js
+++ ASP.NET_Core_Web_Application_CURD/ASP.NET_Core_Web_Application_CURD/wwwroot/js/BookList.js
@@ -37,3 +37,30 @@ function loadDataTable() {
         "width": "100%"
     });
 }
+
+function Delete(url) {
+   
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
